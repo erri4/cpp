c21af0d90b670052e895a46c123a8dbd6d926ba0
rif av <reef.abramson@gmail.com>
2025-08-09 21:13:31 +0300

    now graham's number

--- lester_contest.cpp
+++ lester_contest.cpp
@@ -1,5 +1,35 @@
-#include <limits>
+int pow(int base, int exponent){
+    int r = 1;
+    for (int i = 0; i < exponent; i++){
+        r *= base;
+    }
+    return r;
+}
 
-int main() {
-    return std::numeric_limits<int>::max();
+
+int ntower(int a, int b, int n){
+    /*
+    a^^^^^^...^^^b
+     ------------
+           n
+    */
+    if (n == 1) {
+        return pow(a, b);
+    }
+    if (b == 1) {
+        return a;
+    }
+    return ntower(a, ntower(a, b - 1, n), n - 1);
+}
+
+int graham(int n) {
+    if (n == 1) {
+        return ntower(3, 3, 4);
+    }
+    int prev = graham(n - 1);
+    return ntower(3, 3, prev);
+}
+
+int main(){
+    return graham(64);
 }
